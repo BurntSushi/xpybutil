1e4e38f9f04a488a0a2b5baf10a170db98ddb357
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2011-08-18 09:24:02 -0400

    support multiple keysyms per keycode finally

--- xpybutil/keysym.py
+++ xpybutil/keysym.py
@@ -37,6 +37,21 @@ def parse_buttonstring(button_string):
 
     return mods, button
 
+def lookup_string(c, syms, kstr):
+    assert isinstance(syms, xcb.xproto.GetKeyboardMappingReply)
+
+    if kstr in keysyms:
+        return get_keycode(c, syms, keysyms[kstr])
+    elif len(kstr) > 1 and kstr.capitalize() in keysyms:
+        return get_keycode(c, syms, keysyms[kstr.capitalize()])
+
+    return None
+
+def lookup_keysym(keysym):
+    if keysym in keycodes:
+        return keycodes[keysym][0]
+    return None
+
 def get_min_max_keycode(c):
     return c.get_setup().min_keycode, c.get_setup().max_keycode
 
@@ -55,23 +70,28 @@ def get_keysym(c, syms, keycode, col=0):
 
     mn, mx = get_min_max_keycode(c)
     per = syms.keysyms_per_keycode
+    ind = (keycode - mn) * per + col
 
-    return syms.keysyms[(keycode - mn) * per]
+    return syms.keysyms[ind]
+    # if ind in syms.keysyms: 
+    # return None 
 
-def get_keysym_string(c, syms, keysym):
+def get_keysym_string(c, syms, keysym, col=0):
     assert isinstance(syms, xcb.xproto.GetKeyboardMappingReply)
 
-    return keycodes[keysym][0]
+    return keycodes[keysym][col]
 
 def get_keycode(c, syms, keysym):
     assert isinstance(syms, xcb.xproto.GetKeyboardMappingReply)
 
     res = []
     mn, mx = get_min_max_keycode(c)
-    for j in xrange(mn, mx + 1):
-        ks = get_keysym(c, syms, j)
-        if ks == keysym:
-            return j
+    cols = syms.keysyms_per_keycode
+    for i in xrange(mn, mx + 1):
+        for j in xrange(0, cols):
+            ks = get_keysym(c, syms, i, col=j)
+            if ks == keysym:
+                return i
 
     return None
 
@@ -137,13 +157,12 @@ def ungrab_pointer(c):
     c.core.UngrabPointer(xcb.xproto.Time.CurrentTime)
 
 def grab_keyboard(c, grab_win):
-    c.core.GrabKeyboard(
-        False, grab_win, xcb.xproto.Time.CurrentTime,
-        xcb.xproto.GrabMode.Async, xcb.xproto.GrabMode.Async
-    )
+    return c.core.GrabKeyboard(False, grab_win, xcb.xproto.Time.CurrentTime,
+                               xcb.xproto.GrabMode.Async, 
+                               xcb.xproto.GrabMode.Async).reply()
 
 def ungrab_keyboard(c):
-    c.core.UngrabKeyboard(xcb.xproto.Time.CurrentTime)
+    c.core.UngrabKeyboardChecked(xcb.xproto.Time.CurrentTime).check()
 
 def grab_key(c, wid, modifiers, key):
     try:
@@ -191,17 +210,3 @@ def ungrab_button(c, wid, modifiers, button):
     except xcb.xproto.BadAccess:
         return False
 
-def lookup_string(c, syms, kstr):
-    assert isinstance(syms, xcb.xproto.GetKeyboardMappingReply)
-
-    if kstr in keysyms:
-        return get_keycode(c, syms, keysyms[kstr])
-    elif len(kstr) > 1 and kstr.capitalize() in keysyms:
-        return get_keycode(c, syms, keysyms[kstr.capitalize()])
-
-    return None
-
-def lookup_keysym(keysym):
-    if keysym in keycodes:
-        return keycodes[keysym][0]
-    return None
