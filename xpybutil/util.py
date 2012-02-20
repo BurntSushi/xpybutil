a3df591bd796cc6e206785400184773efa368ac4
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-02-20 15:12:06 -0500

    documented and switch a get_parent_window to window module

--- xpybutil/util.py
+++ xpybutil/util.py
@@ -1,13 +1,27 @@
+"""
+A vast assortment of utility functions. The ones of interest to 
+you are probably 'get_atom' and 'get_atom_name'. The rest are
+heavily used throughout the rest of xpybutil.
+"""
 import struct
 
 import xcb.xproto
 
-from xpybutil import conn, root
+from xpybutil import conn
 
 __atom_cache = {}
 __atom_nm_cache = {}
 
 class Cookie(object):
+    """
+    The base Cookie class. The role of a cookie is to serve as an intermediary
+    between you and the X server. After calling one of the many functions
+    in the ewmh or icccm modules, the X server is typically not contacted
+    immediately. Usually, you'll need to call the 'check()' or 'reply()'
+    methods on the cookie returned by one of the functions in the ewmh or
+    icccm modules. (Alternatively, you could flush the X buffer using
+    ``conn_obj.flush()``.)
+    """
     def __init__(self, cookie):
         self.cookie = cookie
 
@@ -15,10 +29,19 @@ class Cookie(object):
         return self.cookie.check()
 
 class PropertyCookie(Cookie):
+    """
+    A regular property cookie that uses 'get_property_value' to return a nicer
+    version to you. (Instead of raw X data.)
+    """
     def reply(self):
         return get_property_value(self.cookie.reply())
 
 class PropertyCookieSingle(Cookie):
+    """
+    A cookie that should only be used when one is guaranteed a single logical
+    result. Namely, 'get_property_value' will be stupid and return a single
+    list. This class checks for that, and takes the head of that list.
+    """
     def reply(self):
         ret = get_property_value(self.cookie.reply())
 
@@ -27,81 +50,49 @@ class PropertyCookieSingle(Cookie):
         return ret
 
 class AtomCookie(Cookie):
+    """
+    Pulls the ATOM identifier out of the reply object.
+    """
     def reply(self):
         return self.cookie.reply().atom
 
 class AtomNameCookie(Cookie):
+    """
+    Converts the null terminated list of characters (that represents an
+    ATOM name) to a string.
+    """
     def reply(self):
         return str(self.cookie.reply().name.buf())
 
-# Handle atom caching
-def build_atom_cache(atoms):
-    global __atom_cache, __atom_nm_cache
-
-    for atom in atoms:
-        __atom_cache[atom] = get_atom_cookie(atom, only_if_exists=False)
-    for atom in __atom_cache:
-        if isinstance(__atom_cache[atom], AtomCookie):
-            __atom_cache[atom] = __atom_cache[atom].reply()
-
-    __atom_nm_cache = dict((v, k) for k, v in __atom_cache.iteritems())
-
-def get_atom(atom_name, only_if_exists=False):
-    global __atom_cache
-
-    a = __atom_cache.setdefault(atom_name,
-                                get_atom_cookie(atom_name, 
-                                                only_if_exists).reply())
-    if isinstance(a, AtomCookie):
-        a = a.reply()
-
-    return a
-
-def get_atom_name(atom):
-    global __atom_nm_cache
-
-    a = __atom_nm_cache.setdefault(atom, get_atom_name_cookie(atom).reply())
-
-    if isinstance(a, AtomNameCookie):
-        a = a.reply()
-    
-    return a
-
-def get_atom_cookie(atom_name, only_if_exists=False):
-    atom = conn.core.InternAtomUnchecked(only_if_exists, len(atom_name),
-                                         atom_name)
-    return AtomCookie(atom)
-
-def get_atom_name_cookie(atom):
-    return AtomNameCookie(conn.core.GetAtomNameUnchecked(atom))
-
-def get_property(window, atom):
-    if isinstance(atom, basestring):
-        atom = get_atom(atom)
-    return conn.core.GetProperty(False, window, atom,
-                                 xcb.xproto.GetPropertyType.Any, 0,
-                                 2 ** 32 - 1)
-
-def get_property_unchecked(window, atom):
-    if isinstance(atom, basestring):
-        atom = get_atom(atom)
-    return conn.core.GetPropertyUnchecked(False, window, atom,
-                                          xcb.xproto.GetPropertyType.Any, 0,
-                                          2 ** 32 - 1)
-
 def get_property_value(property_reply):
-    assert isinstance(property_reply, xcb.xproto.GetPropertyReply)
+    """
+    A function that takes a property reply object, and turns its value into
+    something nice for us.
 
+    In particular, if the format of the reply is '8', then assume that it 
+    is a string. Moreover, it could be a list of strings that are null
+    terminated. In this case, return a list of Python strings. Otherwise, just
+    convert it to a string and remove the null terminator if it exists.
+
+    Otherwise, the format must be a list of integers that has to be unpacked.
+    Sometimes, these integers are ATOM identifiers, so it is useful to map
+    'get_atom_name' over this list if that's the case.
+
+    :param property_reply: An object returned by a cookie's "reply" method.
+    :type property_reply: xcb.xproto.GetPropertyReply
+    :return: Either a string, a list of strings or a list of integers depending
+             upon the format of the property reply.
+    """
     if property_reply.format == 8:
         if 0 in property_reply.value[:-1]:
             ret = []
-            s = ''
+            s = []
             for o in property_reply.value:
                 if o == 0:
-                    ret.append(s)
-                    s = ''
+                    ret.append(''.join(s))
+                    s = []
                 else:
-                    s += chr(o)
+                    s.append(chr(o))
         else:
             ret = str(property_reply.value.buf())
             if len(property_reply.value) > 0 and 0 == property_reply.value[-1]:
@@ -114,6 +105,134 @@ def get_property_value(property_reply):
 
     return None
 
-def get_parent_window(window):
-    return conn.core.QueryTree(window).reply().parent
+def get_property(window, atom):
+    """
+    Abstracts the process of issuing a GetProperty request.
+
+    You'll typically want to call the ``reply`` method on the return value of
+    this function, and pass that result to
+    'get_property_value' so that the data is nicely formatted.
+
+    :param window: A window identifier.
+    :type window: int
+    :param atom: An atom identifier.
+    :type atom: int OR str
+    :rtype: xcb.xproto.GetPropertyCookie
+    """
+    if isinstance(atom, basestring):
+        atom = get_atom(atom)
+    return conn.core.GetProperty(False, window, atom,
+                                 xcb.xproto.GetPropertyType.Any, 0,
+                                 2 ** 32 - 1)
+
+def get_property_unchecked(window, atom):
+    """
+    Abstracts the process of issuing a GetPropertyUnchecked request.
+
+    You'll typically want to call the ``reply`` method on the return value of
+    this function, and pass that result to
+    'get_property_value' so that the data is nicely formatted.
+
+    :param window: A window identifier.
+    :type window: int
+    :param atom: An atom identifier.
+    :type atom: int OR str
+    :rtype: xcb.xproto.GetPropertyCookie
+    """
+    if isinstance(atom, basestring):
+        atom = get_atom(atom)
+    return conn.core.GetPropertyUnchecked(False, window, atom,
+                                          xcb.xproto.GetPropertyType.Any, 0,
+                                          2 ** 32 - 1)
+
+def build_atom_cache(atoms):
+    """
+    Quickly builds a cache of ATOM names to ATOM identifiers (and the reverse).
+    You'll only need to use this function if you're using atoms not defined in 
+    the ewmh, icccm or motif modules. (Otherwise, those modules will build this 
+    cache for you.)
+
+    The 'get_atom' and 'get_atom_name' function automatically use this cache.
+
+    :param atoms: A list of atom names.
+    :rtype: void
+    """
+    global __atom_cache, __atom_nm_cache
+
+    for atom in atoms:
+        __atom_cache[atom] = __get_atom_cookie(atom, only_if_exists=False)
+    for atom in __atom_cache:
+        if isinstance(__atom_cache[atom], AtomCookie):
+            __atom_cache[atom] = __atom_cache[atom].reply()
+
+    __atom_nm_cache = dict((v, k) for k, v in __atom_cache.iteritems())
+
+def get_atom(atom_name, only_if_exists=False):
+    """
+    Queries the X server for an ATOM identifier using a name. If we've already
+    cached the identifier, then we don't contact the X server.
+
+    If the identifier is not cached, it is added to the cache.
+
+    If 'only_if_exists' is false, then the atom is created if it does not exist
+    already.
+
+    :param atom_name: An atom name.
+    :type atom_name: str
+    :param only_if_exists: If false, the atom is created if it didn't exist.
+    :type only_if_exists: bool
+    :return: ATOM identifier.
+    :rtype: int
+    """
+    global __atom_cache
+
+    a = __atom_cache.setdefault(atom_name,
+                                __get_atom_cookie(atom_name, 
+                                                  only_if_exists).reply())
+    if isinstance(a, AtomCookie):
+        a = a.reply()
+
+    return a
+
+def get_atom_name(atom):
+    """
+    Queries the X server for an ATOM name using the specified identifier. 
+    If we've already cached the name, then we don't contact the X server.
+
+    If the atom name is not cached, it is added to the cache.
+
+    :param atom: An atom identifier.
+    :type atom: int
+    :return: ATOM name.
+    :rtype: str
+    """
+    global __atom_nm_cache
+
+    a = __atom_nm_cache.setdefault(atom, __get_atom_name_cookie(atom).reply())
+
+    if isinstance(a, AtomNameCookie):
+        a = a.reply()
+    
+    return a
+
+def __get_atom_cookie(atom_name, only_if_exists=False):
+    """
+    Private function that issues the xpyb call to intern an atom.
+
+    :type atom_name: str
+    :type only_if_exists: bool
+    :rtype: xcb.xproto.InternAtomCookie
+    """
+    atom = conn.core.InternAtomUnchecked(only_if_exists, len(atom_name),
+                                         atom_name)
+    return AtomCookie(atom)
+
+def __get_atom_name_cookie(atom):
+    """
+    Private function that issues the xpyb call to get an ATOM identifier's name.
+
+    :type atom: int
+    :rtype: xcb.xproto.GetAtomNameCookie
+    """
+    return AtomNameCookie(conn.core.GetAtomNameUnchecked(atom))
 
--- xpybutil/window.py
+++ xpybutil/window.py
@@ -1,14 +1,71 @@
+"""
+A few utility functions related to client windows. In 
+particular, getting an accurate geometry of a client window
+including the decorations (this can vary with the window
+manager). Also, a functon to move and/or resize a window
+accurately by the top-left corner. (Also can change based on
+the currently running window manager.) 
+
+This module also contains a function 'listen' that must be used 
+in order to receive certain events from a window. For example, 
+if you wanted to run 'func' whenever a property on the root 
+window changed, you could do something like:
+
+  ::
+
+    import xpybutil
+    import xpybutil.event as event
+    import xpybutil.ewmh as ewmh
+    import xpybutil.util as util
+    import xpybutil.window as window
+
+    def func(e):
+      if util.get_atom_name(e.atom) == '_NET_ACTIVE_WINDOW':
+        # Do something whenever the active window changes
+        active_window_id = ewmh.get_active_window().reply()
+
+    window.listen(xpybutil.root, 'PropertyChange')
+    event.connect('PropertyNotify', xpybutil.root, func)
+
+The idea here is to tell X that you want events that fall under
+the 'PropertyChange' category. Then you bind 'func' to the 
+particular event 'PropertyNotify'.
+"""
 import xcb.xproto
 
 from xpybutil import conn
-import util
+import xpybutil.ewmh as ewmh
 
 class WindowManagers(object):
+    """
+    A list of window managers that xpybutil is aware of.
+
+    These mostly modify how we determine the size of a client. In particular,
+    KWin's decorations are in the parent of the parent of the client, whereas
+    in Openbox, they are simply in the parent of the client.
+
+    I am not sure whether I have plans to expand this list.
+    """
     Unknown = 0
     Openbox = 1
     KWin = 2
 
 def listen(window, *event_mask_names):
+    """
+    Makes X report events for the masks provided.
+
+    This function must be called in order to get X send you events about a
+    particular window. (i.e., it is not simply enough to call 'event.connect'.)
+
+    This page is required reading if you are to do any event processing:
+    http://tronche.com/gui/x/xlib/events/processing-overview.html
+
+    :param window: Window identifier.
+    :type window: int
+    :param event_mask_names: List of mask names.
+    :type event_mask_names: List of xcb.xproto.EventMask class variable names
+    :rtype: void
+    """
     masks = 0
     for mask_name in event_mask_names:
         assert hasattr(xcb.xproto.EventMask, mask_name)
@@ -17,9 +74,15 @@ def listen(window, *event_mask_names):
     conn.core.ChangeWindowAttributesChecked(window, xcb.xproto.CW.EventMask, 
                                             [masks]).check()
 
-def _get_geometry(win):
-    raw = conn.core.GetGeometry(win).reply()
-    return raw.x, raw.y, raw.width, raw.height
+def get_parent_window(window):
+    """
+    Uses QueryTree() to find the parent of the given window.
+
+    :param window: Window identifier.
+    :return: Parent window identifier of 'window'.
+    :rtype: int
+    """
+    return conn.core.QueryTree(window).reply().parent
 
 def get_geometry(window, window_manager=None):
     """
@@ -29,13 +92,19 @@ def get_geometry(window, window_manager=None):
 
     Since decorations are different for each window manager, you'll get the
     best possible results by passing a supported window manager in.
+
+    :param window: Window identifier.
+    :param window_manager: A class variable from Window.WindowManagers
+    :type window_manager: int
+    :return: Real geometry of a client window starting from the top-left corner.
+    :rtype: (top_left_x, top_left_y, width, height)
     """
 
     if window_manager is WindowManagers.KWin:
-        p = util.get_parent_window(window)
-        return _get_geometry(util.get_parent_window(p))
+        p = get_parent_window(window)
+        return __get_geometry(get_parent_window(p))
     else:
-        return _get_geometry(util.get_parent_window(window))
+        return __get_geometry(get_parent_window(window))
 
 def moveresize(win, x=None, y=None, w=None, h=None, window_manager=None):
     """
@@ -43,22 +112,44 @@ def moveresize(win, x=None, y=None, w=None, h=None, window_manager=None):
     its decorations.
 
     It doesn't rely upon _NET_FRAME_EXTENTS, but instead, uses the actual
-    parent window to adjust the width and height.
-    """
-    import ewmh
+    parent window to adjust the width and height. (I've found _NET_FRAME_EXTENTS
+    to be wildly unreliable.)
 
+    :param win: Window identifier.
+    :param x: Top left x coordinate.
+    :param y: Top left y coordinate.
+    :param w: Client width.
+    :param h: Client height.
+    :param window_manager: A class variable from Window.WindowManagers
+    :type window_manager: int
+    :rtype: void
+    """
     if window_manager is WindowManagers.KWin:
-        tomove = util.get_parent_window(util.get_parent_window(win))
+        tomove = get_parent_window(get_parent_window(win))
     else:
-        tomove = util.get_parent_window(win)
+        tomove = get_parent_window(win)
 
     if tomove:
-        cx, cy, cw, ch = _get_geometry(win)
-        px, py, pw, ph = _get_geometry(tomove)
+        cx, cy, cw, ch = __get_geometry(win)
+        px, py, pw, ph = __get_geometry(tomove)
 
         w -= pw - cw
         h -= ph - ch
 
     ewmh.request_moveresize_window(win, x=x, y=y, width=max(1, w), 
                                    height=max(1, h), source=2)
+
+def __get_geometry(win):
+    """
+    Private function that abstracts the low level GetGeometry call.
+
+    If you're looking for the size of a window including its decorations,
+    please use ``window.get_geometry``.
+
+    :param win: Window identifier.
+    :return: X rectangle of the window.
+    :rtype: (x, y, width, height)
+    """
+    raw = conn.core.GetGeometry(win).reply()
+    return raw.x, raw.y, raw.width, raw.height
 
