700bbe079f4cdbb1aeae26cf639925b5ecb7446a
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-05-08 20:36:46 -0700

    Updated setup script

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
 from __future__ import print_function
 import sys
 
-from distutils import sysconfig
-from distutils.core import setup
+from setuptools import setup
 
 try:
     from xpybutil.compat import xproto, xinerama, randr
 except:
-    print ('')
-    print ('xpybutil requires the X Python Binding')
-    print ('See: http://cgit.freedesktop.org/xcb/xpyb/')
-    print ('More options: xpyb-ng:', 'https://github.com/dequis/xpyb-ng',
-           'and xcffib:', 'https://github.com/tych0/xcffib')
+    print('')
+    print('xpybutil requires the X Python Binding')
+    print('See: http://cgit.freedesktop.org/xcb/xpyb/')
+    print('More options: xpyb-ng:', 'https://github.com/dequis/xpyb-ng',
+          'and xcffib:', 'https://github.com/tych0/xcffib')
     sys.exit(1)
 
 setup(
-    name = "xpybutil",
-    author = "Andrew Gallant",
-    author_email = "andrew@pytyle.com",
-    version = "0.0.1",
-    license = "WTFPL",
-    description = "An incomplete xcb-util port plus some extras",
-    long_description = "See README",
-    url = "http://pytyle.com",
-    platforms = 'POSIX',
-    packages = ['xpybutil'],
-    data_files = [('share/doc/xpybutil', ['README', 'COPYING'])]
+    name="xpybutil",
+    maintainer="Fenner Macrae",
+    author_email="fmacrae.dev@gmail.com",
+    version="0.0.4",
+    license="WTFPL",
+    description="An incomplete xcb-util port plus some extras",
+    long_description="See README",
+    url="http://pytyle.com",
+    platforms='POSIX',
+    packages=['xpybutil'],
+    data_files=[('share/doc/xpybutil', ['README', 'COPYING'])]
 )
